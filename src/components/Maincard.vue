c65432c13b2f469d8d58133d76c5886cd7b1e54d
Honggi Lee <alexhonggi@gmail.com>
2021-07-28 18:17:55 +0900

    accepted incoming changes

--- src/components/Maincard.vue
+++ src/components/Maincard.vue
@@ -5,22 +5,14 @@
       <p @click="show = !show ; pass = true ; pass2 = false ; index++" class = "nextFront" v-bind:key="show" v-if="!show" :style="{backgroundColor:colorlist[index]}"><div class="quote">{{users[index].description}}</div></p>
     </transition>
     <transition name="fade3">
-<<<<<<< HEAD
-      <p class = "nextBack" v-bind:key="show" v-if="!show" :style="{backgroundColor:colorlist[index]}"><div class="name" >{{users[index].name}}</div><div class="job">{{users[index].profession}}</div><div class="topic">{{users[index].dream}}</div></p>
-=======
       <p class = "nextBack" v-bind:key="show" v-if="!show" :style="{backgroundColor:colorlist[index]}"><div class="name">{{users[index].name.split(' ')[0]}}</div><div class="surname">{{users[index].name.split(' ')[1]}}</div><div class="job" >{{users[index].profession}}</div><div class="bar"></div><div class="topic">{{users[index].dream}}</div></p>
->>>>>>> 06fd6fa370159b1cddc73984f0209debf82c5b9a
     </transition>
     <!-- 밑줄에 소개 -->
     <transition name="fade">
       <p @click="show = !show ; pass2 = !pass2 ; pass = false ; index++" class = "nextFront" v-bind:key="show" v-if="show" :style="{backgroundColor:colorlist[index]}"><div class="quote">{{users[index].description}}</div></p>
     </transition>
     <transition name="fade3">
-<<<<<<< HEAD
-      <p class = "nextBack" v-bind:key="show" v-if="show" :style="{backgroundColor:colorlist[index]}"><div class="name" >{{users[index].name}}</div><div class="job">{{users[index].profession}}</div><div class="topic">{{users[index].dream}}</div></p>
-=======
       <p class = "nextBack" v-bind:key="show" v-if="show" :style="{backgroundColor:colorlist[index]}"><div class="name">{{users[index].name.split(' ')[0]}}</div><div class="surname">{{users[index].name.split(' ')[1]}}</div><div class="job">{{users[index].profession}}</div><div class="bar"></div><div class="topic">{{users[index].dream}}</div></p>
->>>>>>> 06fd6fa370159b1cddc73984f0209debf82c5b9a
     </transition>
     <!-- 밑에다 name, job, topic -->
     <transition name="fade2">
